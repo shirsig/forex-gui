63e5040a653272874b8bfc1b5ba4793a16355b90
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2017-09-22 07:43:17 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit  {
+
+  constructor(private http: Http) {
+  }
+
+  ngOnInit(): void {
+    this.transactions = Observable.interval(5000).mergeMap(() => this.http.get("http://localhost:8080/transactions")).map(response => response.json());
+  }
+
+  transactions: Observable<any>;
+
+  username: String;
+  password: String;
+
+  type: string;
+
+  symbol: string;
+  amount: number;
+  side: string;
+  limit: number;
+
+  onSubmit() {
+    alert(this.symbol);
+  }
 }
